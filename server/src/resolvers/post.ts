e9fb270a2d2c4992fc6f61a3290a39d2530fc2c9
muzyk0 <ru9art@gmail.com>
2021-10-24 12:21:44 +0300

    Post resolver relation creator

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -11,6 +11,7 @@ import {
     FieldResolver,
     Root,
     ObjectType,
+    Info,
 } from "type-graphql";
 import { getConnection } from "typeorm";
 
@@ -50,18 +51,45 @@ export class PostResolver {
     ): Promise<PaginatedPosts> {
         const realLimit = Math.min(50, limit);
         const realLimitPlusOne = realLimit + 1;
-        const qb = getConnection()
+
+        const replacements: any[] = [realLimitPlusOne];
+
+        if (cursor) {
+            replacements.push(new Date(parseInt(cursor)));
+        }
+
+        const posts = await getConnection().query(
+            `
+        select p.*,
+        json_build_object(
+          'id', u.id,
+          'username', u.username,
+          'email', u.email,
+          'createdAt', u."createdAt",
+          'updatedAt', u."updatedAt"
+          ) creator
+        from post p
+        inner join public.user u on u.id = p."creatorId"
+        ${cursor ? `where p."createdAt" < $2` : ""}
+        order by p."createdAt" DESC
+        limit $1
+        `,
+            replacements
+        );
+
+        /* const qb = getConnection()
             .getRepository(Post)
             .createQueryBuilder("p")
-            .orderBy('"createdAt"', "DESC")
+            .innerJoinAndSelect("p.creator", "u", 'u.id = p."creatorId"')
+            .orderBy('p."createdAt"', "DESC")
             .take(realLimitPlusOne);
 
         if (cursor) {
-            qb.where('"createdAt" < :cursor', {
+            qb.where('p."createdAt" < :cursor', {
                 cursor: new Date(parseInt(cursor)),
             });
         }
-        const posts = await qb.getMany();
+        const posts = await qb.getMany(); */
         return {
             posts: posts.slice(0, realLimit),
             hasMore: posts.length === realLimitPlusOne,
@@ -70,7 +98,9 @@ export class PostResolver {
 
     @Query(() => Post, { nullable: true })
     async post(@Arg("id", () => Int) id: number): Promise<Post | undefined> {
-        return Post.findOne(id);
+        return Post.findOne(id, {
+            relations: ["creator"],
+        });
     }
 
     @Mutation(() => Post)
